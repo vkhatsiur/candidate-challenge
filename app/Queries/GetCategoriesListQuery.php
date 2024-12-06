3090cfb6560c06872f6dcb5b8a921477704a9934
Vasyl Khatsiur <khatsiur.vasyl@homsters.com>
2024-12-06 15:05:03 +0200

    add categories to cache

--- app/Queries/GetCategoriesListQuery.php
+++ app/Queries/GetCategoriesListQuery.php
@@ -7,6 +7,7 @@ use App\CommandBus\IQueryHandler;
 use App\Dtos\CategoryDto;
 use App\Models\Category;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 
 class GetCategoriesListQuery implements IQuery
@@ -19,8 +20,10 @@ class GetCategoriesListQueryHandler implements IQueryHandler
 
     public function handle(GetCategoriesListQuery $query) : Collection
     {
-        return Category::all()->map(function ($category) {
-            return CategoryDto::create($category->name,Storage::disk('categories')->url($category->logo), $category->slug);
+        return Cache::remember('categories', 60, function () {
+            return Category::all()->map(function ($category) {
+                return CategoryDto::create($category->name, Storage::disk('categories')->url($category->logo), $category->slug);
+            });
         });
     }
 }
